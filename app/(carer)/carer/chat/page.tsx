7655d1c27791abf842f6664573d291710b5855ea
tintin4303 <nyunt.tin4303@gmail.com>
2026-02-26 23:04:35 +0700

    fixed carer chat

--- app/(carer)/carer/chat/page.tsx
+++ app/(carer)/carer/chat/page.tsx
@@ -4,7 +4,8 @@ import ChatInterface from '@/components/dashboard/ChatInterface';
 
 export default function CarerChatPage() {
     return (
-        <div className="h-full">
+        // Escape the parent layout's padded wrapper to go full-bleed
+        <div className="-mx-4 sm:-mx-6 md:-mx-8 -my-6 h-[calc(100vh-4rem)] pb-16 md:pb-0">
             <ChatInterface />
         </div>
     );
